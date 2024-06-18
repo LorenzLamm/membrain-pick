df2c906b4f3161a17455c9aa8f97ac8162fc3dfd
LorenzLamm <lorenz.lamm@googlemail.com>
2024-06-18 13:55:28 +0200

    unify scalings

--- src/membrain_pick/compute_mesh_projection.py
+++ src/membrain_pick/compute_mesh_projection.py
@@ -11,8 +11,6 @@ from skimage import measure
 
 from scipy.ndimage import map_coordinates
 
-# import trimesh
-
 
 def convert_seg_to_mesh(
     seg: np.ndarray, smoothing: int, voxel_size: float = 1.0
@@ -100,24 +98,52 @@ def get_tomo_values_along_normal(point, normal, tomo, b_splines=True, steps=(-6,
     return values
 
 
-def compute_positions_along_normals(mesh, steps=(-6, 7), step_size=0.25, verts=None, normals=None, input_pixel_size=14.08, output_pixel_size=10.0):
+def compute_positions_along_normals(
+        mesh, #TODO: clean up
+        steps=(-6, 7), 
+        step_size=2.5, # in Angstrom 
+        verts=None, 
+        normals=None, 
+        input_pixel_size=14.08, 
+        output_pixel_size=10.0): #TODO: remove
     # Get vertices and triangle combinations
     if verts is None:
         verts = mesh.points
     if normals is None:
         normals = mesh.point_normals
 
-    positions = verts[:, None, :] + normals[:, None, :] * np.arange(steps[0], steps[1])[None, :, None] * step_size
+    tomo_step_size = step_size / input_pixel_size
 
-    # Go back to original pixel size to match tomogram
-    pixel_factor = input_pixel_size / output_pixel_size
-    positions = positions / pixel_factor
+    positions = verts[:, None, :] + normals[:, None, :] * np.arange(steps[0], steps[1])[None, :, None] * tomo_step_size
+
+    # # Go back to original pixel size to match tomogram #TODO: remove
+    # pixel_factor = input_pixel_size / output_pixel_size #TODO: remove
+    # positions = positions / pixel_factor #TODO: remove
     return positions
 
 
-def compute_values_along_normals(mesh, tomo, b_splines=True, steps=(-6, 7), input_pixel_size=14.08, output_pixel_size=10., step_size=0.25, verts=None, normals=None):
+def compute_values_along_normals(
+        mesh, #TODO: remove
+        tomo, 
+        steps=(-6, 7), 
+        input_pixel_size=10., 
+        output_pixel_size=10., 
+        step_size=2.5, # in Angstrom
+        verts=None, 
+        normals=None
+        ):
     # Get vertices and triangle combinations
-    positions = compute_positions_along_normals(mesh, steps=steps, step_size=step_size, verts=verts, normals=normals, input_pixel_size=input_pixel_size, output_pixel_size=output_pixel_size)
+    positions = compute_positions_along_normals(
+        mesh, 
+        steps=steps, 
+        step_size=step_size, 
+        verts=verts, 
+        normals=normals, 
+        input_pixel_size=input_pixel_size, 
+        output_pixel_size=output_pixel_size
+        )
+    
+    # Get values along normals
     positions_shape = positions.shape
     positions = positions.reshape(-1, 3)
     positions_transposed = positions.T
@@ -126,93 +152,35 @@ def compute_values_along_normals(mesh, tomo, b_splines=True, steps=(-6, 7), inpu
     return normal_values
 
 
-def correct_normals(mesh):
-    vertices = mesh.points
-    faces = mesh.faces.reshape((-1, 4))[:, 1:4]  # Reshape faces and remove the leading count
-
-    # Create a trimesh mesh using the vertices and faces from PyVista
-    mesh_trimesh = trimesh.Trimesh(vertices=vertices, faces=faces)
-
-    broken = trimesh.repair.broken_faces(mesh_trimesh, color=None)
-
-    # Repair the mesh
-    trimesh.repair.fill_holes(mesh_trimesh)
-
-    # Repair the mesh
-    trimesh.repair.fix_inversion(mesh_trimesh, multibody=True)
-
-    # Now you can use trimesh's functionalities
-    trimesh.repair.fix_normals(mesh_trimesh, multibody=True)
-
-    # If you want to convert it back to PyVista mesh after manipulation
-    mesh = pv.PolyData(mesh_trimesh.vertices, np.hstack([np.full((mesh_trimesh.faces.shape[0], 1), 3, dtype=int), mesh_trimesh.faces]))
-
-    return mesh
-
-
-
-def convert_seg_to_evenly_spaced_mesh(seg, smoothing=2000, barycentric_area=10, normals_correction=False, input_pixel_size=14.08, output_pixel_size=10.0):
-    mesh = convert_seg_to_mesh(seg=seg,
-                        smoothing=smoothing,
-                        voxel_size=1.0)
+def convert_seg_to_evenly_spaced_mesh(
+        seg, 
+        smoothing=2000, 
+        barycentric_area=10, 
+        was_rescaled=False,
+        input_pixel_size=14.08, # TODO: remove
+        output_pixel_size=10.0 # TODO: remove
+        ):
+    # Convert segmentation to mesh using marching cubes
+    mesh = convert_seg_to_mesh(
+        seg=seg,
+        smoothing=smoothing,
+        )
     mesh = mesh.decimate(0.5)
     
-    pixel_factor = input_pixel_size / output_pixel_size
-    mesh.points = mesh.points * pixel_factor * 2
+    # pixel_factor = input_pixel_size / output_pixel_size #TODO:remove
+    # mesh.points = mesh.points * pixel_factor * 2 #TODO: remove
+    if was_rescaled:
+        mesh.points = mesh.points * 2 # rescale to original size
 
+    mesh.points *= input_pixel_size # rescale to phyiscal size to compute barycentric area
+
+    # Remesh the mesh to have evenly spaced triangles
     cluster_points = int(mesh.area / barycentric_area)
     clus = pyacvd.Clustering(mesh)
     clus.subdivide(3)
     clus.cluster(cluster_points)
     remesh = clus.create_mesh()
-    if normals_correction:
-        remesh = correct_normals(remesh)
+
+    remesh.points /= input_pixel_size # rescale back to original size
 
     return remesh
-
-
-def main():
-    path_to_seg = "/scicore/home/engel0006/GROUP/pool-engel/Lorenz/2D_projections/data_Sofie/230425_tomo3_cc_cropped_mb.mrc"
-    path_to_tomo = "/scicore/home/engel0006/GROUP/pool-engel/Lorenz/2D_projections/data_Sofie/230425_tomo3_cc_cropped.rec"
-    out_mesh = "/scicore/home/engel0006/GROUP/pool-engel/Lorenz/2D_projections/data_Sofie/230425_tomo3_cc_cropped_mb_mesh.vtp"
-    out_mesh_vox = "/scicore/home/engel0006/GROUP/pool-engel/Lorenz/2D_projections/data_Sofie/230425_tomo3_cc_cropped_mb_mesh_voxelized.vtp"
-    out_csv = "/scicore/home/engel0006/GROUP/pool-engel/Lorenz/2D_projections/data_Sofie/230425_tomo3_cc_cropped_mb_mesh.csv"
-    tomo = load_tomogram(path_to_tomo).data
-    # seg = load_tomogram(path_to_seg).data
-    # mesh = convert_seg_to_mesh(seg=seg,
-    #                     smoothing=1000,
-    #                     voxel_size=1.0)
-    # mesh = mesh.decimate(0.9)
-    # mesh.save(out_mesh)
-    # exit()
-    # read mesh in again
-    mesh = pv.read(out_mesh)
-    clus = pyacvd.Clustering(mesh)
-    clus.subdivide(3)
-    clus.cluster(200000)
-
-    remesh = clus.create_mesh()
-    remesh.save(out_mesh_vox)
-    
-    mesh = remesh
-    # Get vertices and triangle combinations
-    verts = mesh.points
-    faces = mesh.faces.reshape(-1, 4)[:, 1:]
-    normals = mesh.point_normals
-
-    print(verts.shape, faces.shape, normals.shape)
-
-    normal_values = []
-    time_zero = time()
-    for i in range(len(verts)):
-        if i % 500 == 0:
-            print(i, "/", len(verts), time() - time_zero)
-        norm_vals = get_tomo_values_along_normal(verts[i], normals[i], tomo, b_splines=True)
-        normal_values.append(norm_vals)
-    print("Computing values took", time()-time_zero, "Bsplines were computed?", True)
-    normal_values = np.stack(normal_values, axis=0)
-    point_data = np.concatenate((verts, normals, normal_values), axis=1)
-    store_array_in_csv(out_csv, point_data)
-
-if __name__ == "__main__":
-    main()
